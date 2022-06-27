a0a6bf56198fee033fe4813f555ef867c213253d
BorisIvanov24 <107943008+BorisIvanov24@users.noreply.github.com>
2022-06-27 19:20:13 +0300

    first 2 layers

---
+++ Terrain Generation/Terrain Generation/Program.cs
@@ -0,0 +1,32 @@
+﻿using Raylib_cs;
+
+namespace HelloWorld
+{
+    static class Program
+    {
+
+        static Image world;
+
+        public static void Main()
+        {
+            Raylib.InitWindow(800, 480, "Hello World");
+            
+            
+            world = Raylib.GenImageColor(900, 900, Color.WHITE);
+            
+            unsafe
+            {
+                fixed (Image *vp = &world)
+                {
+                    Raylib.ImageDrawRectangle(vp, 0, 0, 900, 100, Color.BROWN);
+                    Raylib.ImageDrawRectangle(vp, 0, 100, 900, 800, Color.GRAY);
+                }
+             }
+
+            Raylib.ExportImage(world, "world.png");
+            
+
+            Raylib.CloseWindow();
+        }
+    }
+}
